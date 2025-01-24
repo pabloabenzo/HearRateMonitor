d5439f0119c8f7b970331f0311a35de6fb2f2b6d
Pablo Benzo <>
2025-01-24 00:18:05 -0300

    Project upload.

---
+++ HeartRateMonitor/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  HRMViewController.swift
+//  HeartRateMonitor
+//
+//  Created by Pablo Benzo on 22/10/2024.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+}
